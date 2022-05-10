bcf1dbc4be266465f38583d226f4683d8938dbcc
Yura Golubev <gol.y@mail.ru>
2022-05-10 10:53:42 +0300

    first jen

---
+++ src/main/java/com/epam/training/Main.java
@@ -0,0 +1,12 @@
+package com.epam.training;
+
+public class Main {
+    /**
+     * The start method.
+     *  @param args string array.
+     */
+
+    public static void main(final String[] args) {
+        System.out.println("Hello from Git and Jenkins");
+    }
+}
